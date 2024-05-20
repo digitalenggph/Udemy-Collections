83d3ab55f095e976db1c7a1248c7934ffb2a0b40
digitalenggph <karen.lovedorial@gmail.com>
2024-05-20 23:18:30 +0800

    created main_dict_comprehension.py

---
+++ 100 Days of Code - The Complete Python Pro Bootcamp/day-26/day-26-start/main_dict_comprehension.py
@@ -0,0 +1,12 @@
+"""
+DAY-26-NOTES:
+Dictionary Comprehension
+
+new_dict = {new_key:new_value for item in list}
+new_dict = {new_key:new_Value for (key, value) in dict.items()}
+
+
+Conditional Dictionary Comprehension
+new_dict = {new_key:new_value for (key, value) in dict.items() if test}
+
+"""
