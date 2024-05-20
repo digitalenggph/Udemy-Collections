2d0142e90c1be2a97afb306c409fc39c1998aef7
digitalenggph <karen.lovedorial@gmail.com>
2024-05-20 23:49:36 +0800

    finished main_dict_comprehension.py notes and code

--- 100 Days of Code - The Complete Python Pro Bootcamp/day-26/day-26-start/main_dict_comprehension.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/day-26/day-26-start/main_dict_comprehension.py
@@ -10,3 +10,16 @@ Conditional Dictionary Comprehension
 new_dict = {new_key:new_value for (key, value) in dict.items() if test}
 
 """
+
+import random
+
+names = ['Alex', 'Beth', 'Caroline', 'Dave', 'Eleanor', 'Freddie']
+student_scores = {student: random.randint(1, 100) for student in names}
+
+print(student_scores)
+
+# loop through dictionary
+
+passed_students = {student: score for (student, score) in student_scores.items() if score >= 60}
+print(passed_students)
+
