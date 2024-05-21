a18bda05188aa84e8afb20e821eeef11ac28a9b6
digitalenggph <karen.lovedorial@gmail.com>
2024-05-21 15:39:47 +0800

    added notes

--- 100 Days of Code - The Complete Python Pro Bootcamp/day-27/playground.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/day-27/playground.py
@@ -1,3 +1,19 @@
+"""
+DAY-27-NOTES:
+Unlimited arguments
+
+def add(n1, n2):
+    return n1 + n2
+
+for unlimited n,
+
+def add(*args):
+    for n in args:
+        print(n)
+
+"""
+
+
 def add(*numbers):
     return sum(numbers)
 
