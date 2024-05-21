6a4f107530d663df3bdd07b88342eb7d6f40f364
digitalenggph <karen.lovedorial@gmail.com>
2024-05-21 16:02:28 +0800

    added **kwargs notes

--- 100 Days of Code - The Complete Python Pro Bootcamp/day-27/playground.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/day-27/playground.py
@@ -12,6 +12,10 @@ def add(*args):
     for n in args:
         print(n)
 
+
+**kwargs
+arbitrary number of keyword arguments
+
 """
 
 
@@ -21,4 +25,38 @@ def add(*numbers):
 
 sum_playground = add(1, 2, 3)
 
-print(sum_playground)
+
+# print(sum_playground)
+
+def calculate(n, **kwargs):
+    print(kwargs)
+    # for key, value in kwargs.items():
+    #     print(key)
+    #     print(value)
+
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+
+    print(n)
+
+
+calculate(2, add=3, multiply=5)
+
+
+class Car:
+
+    def __init__(self, **kwargs):
+        # will return error if values inside square bracket is not defined
+        # self.make = kwargs["make"]
+        # self.model = kwargs["model"]
+
+        # solution: will just return None
+        self.make = kwargs.get("make")
+        self.model = kwargs.get("model")
+
+
+# my_car = Car(make="Nissan", model="GT-R")
+my_car = Car()
+print(my_car.model)
+
+
