d63960a4793172772db6099b5f5811acf3abfd05
digitalenggph <karen.lovedorial@gmail.com>
2024-05-23 17:05:39 +0800

    created copy of day-29 password generator challenge

---
+++ 100 Days of Code - The Complete Python Pro Bootcamp/day-30/day-30-password-manager-challenge/password_generator.py
@@ -0,0 +1,25 @@
+# Password Generator Project
+# from Day-5-project
+
+from random import randint, shuffle, choice
+
+
+def password_generator():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v',
+               'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+               'R',
+               'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_list = []
+
+    password_list += [str(choice(letters)) for _ in range(randint(8, 10))]
+    password_list += [str(choice(symbols)) for _ in range(randint(2, 4))]
+    password_list += [str(choice(numbers)) for _ in range(randint(2, 4))]
+
+    shuffle(password_list)
+    password = "".join(password_list)
+
+    return password
