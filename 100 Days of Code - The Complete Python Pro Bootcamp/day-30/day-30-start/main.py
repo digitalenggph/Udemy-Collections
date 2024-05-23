7ce19fb39850ecc110b6116b3c0b4dd733baea21
digitalenggph <karen.lovedorial@gmail.com>
2024-05-23 16:07:01 +0800

    finished first part of the lesson with demo

--- 100 Days of Code - The Complete Python Pro Bootcamp/day-30/day-30-start/main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/day-30/day-30-start/main.py
@@ -24,3 +24,20 @@ print(text + 5)
     * finally   - do this no matter what happens
 """
 
+# FileNotFound
+try:
+    file = open("a_file.txt")
+    a_dictionary = {"key": "value"}
+    print(a_dictionary["not a key"])
+except FileNotFoundError:
+    file = open("a_file.txt", mode="w")
+    file.write("Something")
+    print(f"generated a_file.txt")
+except KeyError as error_message:
+    print(f"The key {error_message} does not exist")
+else:
+    content = file.read()
+    print(content)
+finally:
+    file.close()
+    print("File was closed.")
