ad9aac7470844a3116929f01fa2d6fc4ec9eaa05
digitalenggph <karen.lovedorial@gmail.com>
2024-05-23 17:02:15 +0800

    finished adding error handling blocks for main.py from NATO challenge day-26

--- 100 Days of Code - The Complete Python Pro Bootcamp/day-30/day-30-NATO-alphabet-challenge/main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/day-30/day-30-NATO-alphabet-challenge/main.py
@@ -1,31 +1,35 @@
 import pandas as pd
 
-# TODO 1. Create a dictionary in this format:
-"""
-METHOD 1
-phonetic_df = pd.read_csv("nato_phonetic_alphabet.csv", index_col=0)
-phonetic_df_transposed = phonetic_df.transpose()
-phonetic_dict = phonetic_df_transposed.to_dict('list')
-
-dict_final = {letter: phonetic[0] for (letter, phonetic) in phonetic_dict.items()}
-
-phonetic_df_transposed = phonetic_df.transpose()
-phonetic_dict = phonetic_df_transposed.to_dict('list')
-
-dict_final = {letter: phonetic[0] for (letter, phonetic) in phonetic_dict.items()}
-
-print(dict_final)
-
-"""
-
-# Expected method in the exercise
-
 df = pd.read_csv("nato_phonetic_alphabet.csv")
 dict_final = {row.letter: row.code for (index, row) in df.iterrows()}
 print(dict_final)
 
-# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+# my solution
 word = input("Input your word! \n")
-phonetic_list = [dict_final[letter.upper()] for letter in word]
 
+
+def search_dict(character):
+    try:
+        return dict_final[character.upper()]
+    except KeyError:
+        return "_"
+
+
+phonetic_list = [search_dict(letter) for letter in word]
 print(phonetic_list)
+
+
+# instructor's solution
+
+def generate_phonetic():
+    word_ = input("Enter a word: ").upper()
+    try:
+        output_list = [dict_final[letter] for letter in word_]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(output_list)
+
+
+generate_phonetic()
