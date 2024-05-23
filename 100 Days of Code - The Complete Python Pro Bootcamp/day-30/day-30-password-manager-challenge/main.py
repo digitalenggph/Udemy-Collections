aa88ab750815d72ddf0a0255e0371bf0acdc14fe
digitalenggph <karen.lovedorial@gmail.com>
2024-05-23 19:28:09 +0800

    finished main.py lessons for upgraded password manager challenge mini-app

--- 100 Days of Code - The Complete Python Pro Bootcamp/day-30/day-30-password-manager-challenge/main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/day-30/day-30-password-manager-challenge/main.py
@@ -2,7 +2,35 @@ from tkinter import *  # classes only
 from tkinter import messagebox  # modules of code
 from password_generator import password_generator
 import pyperclip
+import json
 
+
+# ---------------------------- SEARCH WEBSITE ------------------------------- #
+
+def search_website():
+    website = website_textbox.get()
+    if len(website) !=0:
+        try:
+            with open("data.json", mode='r') as data_file:
+                # reads old data
+                data = json.load(data_file)
+                # retrieves data from json
+                email = data[website]["email"]
+                password = data[website]["password"]
+        except FileNotFoundError:
+            messagebox.showinfo(title="No file yet.",
+                                message="No file yet.")
+        except KeyError:
+            messagebox.showinfo(title="Website not found",
+                                message=f"No entries found for website {website}")
+        else:
+            messagebox.showinfo(
+                title=website,
+                message=f"Email: {email} \nPassword: {password}"
+            )
+    else:
+        messagebox.showinfo(title="Empty website",
+                            message="Input website in the entry box please.")
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
@@ -14,12 +42,21 @@ def generate_password():
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
-    # concatenate data strings
-    data = f"{website_textbox.get()} | {email_user_textbox.get()} | {password_textbox.get()}\n"
+    website = website_textbox.get()
+    email_user = email_user_textbox.get()
+    password = password_textbox.get()
 
-    if len(website_textbox.get()) == 0 or \
-            len(email_user_textbox.get()) == 0 or \
-            len(password_textbox.get()) == 0:
+    # data_string = f"{website} | {email_user} | {password}\n"
+    new_data = {
+        website: {
+            "email": email_user,
+            "password": password,
+        }
+    }
+
+    if len(website) == 0 or \
+            len(email_user) == 0 or \
+            len(password) == 0:
 
         messagebox.showerror(
             title="Oops",
@@ -27,25 +64,26 @@ def save():
         )
 
     else:
-        # messagebox
-        # messagebox.showinfo(title="Title", message="Message")
-        is_ok = messagebox.askokcancel(
-            title=website_textbox.get(),
-            message=f"These are the details entered: \
-                    \nEmail: {email_user_textbox.get()} \
-                    \nPassword: {password_textbox.get()} \
-                    \nIs it OK to save?"
-        )
+        try:
+            with open("data.json", mode='r') as data_file:
+                # reads old data
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", mode='w') as data_file:
+                # creates new data
+                # first input
+                json.dump(new_data, data_file, indent=4)
+        else:
+            # updates old data with new data
+            data.update(new_data)
 
-        if is_ok:
-            # append string to data.txt file
-            # with open to automatically open then close the file
-            with open("data.txt", mode='a') as file:
-                file.write(data)
-
-            # reset all textboxes
+            with open("data.json", mode='w') as data_file:
+                # saves updated data
+                json.dump(data, data_file, indent=4)
+        finally:
             website_textbox.delete(0, END)
             password_textbox.delete(0, END)
+            print(data)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -67,9 +105,9 @@ canvas.grid(column=1, row=0)
 website_label = Label(text="Website: ")
 website_label.grid(column=0, row=1)
 
-website_textbox = Entry(width=35)
+website_textbox = Entry(width=21)
 website_textbox.focus()
-website_textbox.grid(column=1, row=1, columnspan=2, sticky="EW")
+website_textbox.grid(column=1, row=1, columnspan=1, sticky="EW")
 
 # email/username
 email_user_label = Label(text="Email/Username: ")
@@ -87,11 +125,14 @@ password_label.grid(column=0, row=3)
 password_textbox = Entry(width=21)
 password_textbox.grid(column=1, row=3, sticky="EW")
 
-# generate password button
+# generate buttons
 generate_button = Button(text="Generate Password", command=generate_password)
 generate_button.grid(column=2, row=3, sticky="EW")
 
 add_button = Button(text="Add", width=36, command=save)
 add_button.grid(column=1, row=4, columnspan=2, sticky="EW")
+
+search_button = Button(text="Search", command=search_website)
+search_button.grid(column=2, row=1, sticky="EW")
 
 window.mainloop()
