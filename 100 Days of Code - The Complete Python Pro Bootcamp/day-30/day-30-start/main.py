2fa8f89d8cba7fd36e40b528cb99aacd75f74eba
digitalenggph <karen.lovedorial@gmail.com>
2024-05-23 15:56:10 +0800

    created working directory and added starting main.py file for day-30

---
+++ 100 Days of Code - The Complete Python Pro Bootcamp/day-30/day-30-start/main.py
@@ -0,0 +1,26 @@
+"""
+DAY-30-NOTES:
+
+# gets traceback error "No such file or directory"
+with open("a_file.txt") as file:
+    file.read()
+
+# KeyError
+a_dictionary = {"key": "value"}
+value = a_dictionary["non_existent_key"]
+
+# IndexError
+fruit_list = ["Apple", "Banana", "Pear"]
+fruit = fruit_list[3]
+
+# TypeError
+text = "abc"
+print(text + 5)
+
+# try-except-else-finally
+    * try       - something that might cause an exception
+    * except    - do this if there was an exception
+    * else      - do this if there were no exceptions
+    * finally   - do this no matter what happens
+"""
+
