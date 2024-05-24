2e1eb010b465cf136d67d61ba8707eb630bfb9a4
digitalenggph <karen.lovedorial@gmail.com>
2024-05-24 18:50:35 +0800

    finished randomizing "key" word to be translated

--- 100 Days of Code - The Complete Python Pro Bootcamp/day-31/main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/day-31/main.py
@@ -3,6 +3,8 @@ from tkinter import *
 import pandas as pd
 from random import choice
 
+global random_word
+
 BACKGROUND_COLOR = "#B1DDC6"
 FONT_NAME = "Arial"
 
@@ -12,13 +14,26 @@ data_source_file = "./data/french_words.csv"
 data_df = pd.read_csv(data_source_file)
 data_dict = data_df.to_dict(orient="records")
 
-random_word = choice(data_dict)
 
-# gets languages
-# in case the data is not just French words
-lang_list = [language for language in random_word]
-title = lang_list[0]
-word = random_word[title]
+def refresh_card():
+    global random_word
+    random_word = choice(data_dict)
+    # in case the data is not just French words
+    lang_list = [language for language in random_word]
+    key = lang_list[0]
+    canvas.itemconfig(title_text, text=key)
+    canvas.itemconfig(card_text, text=random_word[key])
+
+
+# -------------------------- BUTTON SETUP ------------------------------ #
+
+def known_word():
+    refresh_card()
+
+
+def unknown_word():
+    refresh_card()
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -34,17 +49,21 @@ canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.create_image(400, 263, image=card_front_img)
 
 # canvas text
-title_text = canvas.create_text(400, 150, text=title, fill="black", font=(FONT_NAME, 40, "italic"))
-card_text = canvas.create_text(400, 263, text=word, fill='black', font=(FONT_NAME, 50, "bold"))
+
+title_text = canvas.create_text(400, 150, text="", fill="black", font=(FONT_NAME, 40, "italic"))
+card_text = canvas.create_text(400, 263, text="", fill='black', font=(FONT_NAME, 50, "bold"))
 canvas.grid(row=0, column=0, columnspan=2)
 
 # button config
+
 unknown_button_img = PhotoImage(file="./images/wrong.png")
-unknown_button = Button(image=unknown_button_img, borderwidth=0, highlightthickness=0)
+unknown_button = Button(image=unknown_button_img, command=unknown_word, borderwidth=0, highlightthickness=0)
 unknown_button.grid(row=1, column=0)
 
 known_button_img = PhotoImage(file="./images/right.png")
-known_button = Button(image=known_button_img, borderwidth=0, highlightthickness=0)
+known_button = Button(image=known_button_img, command=known_word, borderwidth=0, highlightthickness=0)
 known_button.grid(row=1, column=1)
+
+refresh_card()
 
 window.mainloop()
