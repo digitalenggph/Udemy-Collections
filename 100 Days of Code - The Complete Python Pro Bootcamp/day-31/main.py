41c53f9a199b1eeeff6fe3d66007b3576fbef547
digitalenggph <karen.lovedorial@gmail.com>
2024-05-24 17:51:39 +0800

    set up source file for language translation

--- 100 Days of Code - The Complete Python Pro Bootcamp/day-31/main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/day-31/main.py
@@ -1,7 +1,24 @@
 from tkinter import *
 
+import pandas as pd
+from random import choice
+
 BACKGROUND_COLOR = "#B1DDC6"
 FONT_NAME = "Arial"
+
+# --------------------------- DATA SETUP ------------------------------ #
+
+data_source_file = "./data/french_words.csv"
+data_df = pd.read_csv(data_source_file)
+data_dict = data_df.to_dict(orient="records")
+
+random_word = choice(data_dict)
+
+# gets languages
+# in case the data is not just French words
+lang_list = [language for language in random_word]
+title = lang_list[0]
+word = random_word[title]
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -17,8 +34,8 @@ canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.create_image(400, 263, image=card_front_img)
 
 # canvas text
-title_text = canvas.create_text(400, 150, text="Title", fill="black", font=(FONT_NAME, 40, "italic"))
-card_text = canvas.create_text(400, 263, text="Word", fill='black', font=(FONT_NAME, 50, "bold"))
+title_text = canvas.create_text(400, 150, text=title, fill="black", font=(FONT_NAME, 40, "italic"))
+card_text = canvas.create_text(400, 263, text=word, fill='black', font=(FONT_NAME, 50, "bold"))
 canvas.grid(row=0, column=0, columnspan=2)
 
 # button config
