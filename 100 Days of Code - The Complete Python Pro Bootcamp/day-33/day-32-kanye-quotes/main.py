11a08b0114f61d0237180cfdecad3e69483d5790
digitalenggph <karen.lovedorial@gmail.com>
2024-05-26 00:31:19 +0800

    finished easy-peasy API exercise

--- 100 Days of Code - The Complete Python Pro Bootcamp/day-33/day-32-kanye-quotes/main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/day-33/day-32-kanye-quotes/main.py
@@ -1,10 +1,16 @@
 from tkinter import *
+import requests
+
+URL = "https://api.kanye.rest/"
 
 
 def get_quote():
-    pass
-    #Write your code here.
-
+    response = requests.get(url=URL)
+    response.raise_for_status()
+    quote_json = response.json()
+    quote = quote_json["quote"]
+    canvas.itemconfig(quote_text, text=quote)
+    # Write your code here.
 
 
 window = Tk()
@@ -14,13 +20,13 @@ window.config(padx=50, pady=50)
 canvas = Canvas(width=300, height=414)
 background_img = PhotoImage(file="background.png")
 canvas.create_image(150, 207, image=background_img)
-quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 30, "bold"), fill="white")
+quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 30, "bold"),
+                                fill="white")
 canvas.grid(row=0, column=0)
 
 kanye_img = PhotoImage(file="kanye.png")
 kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
 kanye_button.grid(row=1, column=0)
 
-
-
+get_quote()
 window.mainloop()
