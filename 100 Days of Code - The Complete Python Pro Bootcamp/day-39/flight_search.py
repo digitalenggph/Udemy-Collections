aebc66a21516966a46b0c215a4ca5fb6fbc337ad
digitalenggph <karen.lovedorial@gmail.com>
2024-05-29 22:35:31 +0800

    done coding for method "iata_code" which returns the iata city code (input: city)

--- 100 Days of Code - The Complete Python Pro Bootcamp/day-39/flight_search.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/day-39/flight_search.py
@@ -1,3 +1,39 @@
+import os
+import requests
+
+# ----------------------FLIGHT SEARCH DETAILS----------------------#
+ENDPOINT_HOST = "sky-scanner3.p.rapidapi.com"
+API_KEY = os.environ.get("ENV_RAPIDAPI_KEY")
+# -----------------------------------------------------------------#
+
+endpoint_auto_complete = f"https://{ENDPOINT_HOST}/flights/auto-complete"
+headers = {
+    "X-RapidAPI-Key": API_KEY,
+    "X-RapidAPI-Host": ENDPOINT_HOST,
+}
+
+
 class FlightSearch:
-    #This class is responsible for talking to the Flight Search API.
-    pass
+    def __init__(self):
+        self.endpoint = endpoint_auto_complete
+
+    def iata_code(self, city):
+        querystring = {
+            "query": city,
+            "placeType": "CITY",
+        }
+
+        request = requests.get(url=self.endpoint,
+                               params=querystring,
+                               headers=headers)
+        request.raise_for_status()
+        response = request.json()
+        iata = response['data'][0]['presentation']['skyId']
+        return iata
+
+    def search_flight(self):
+        pass
+
+# -------------------TEST IF CLASS WORKS-------------------#
+# trial = FlightSearch("Amsterdam")
+# print(trial.iata_code())
