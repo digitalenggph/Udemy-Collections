4e85ab05049616f3f308c5cb2708aa7375e5f459
digitalenggph <karen.lovedorial@gmail.com>
2024-06-02 22:41:17 +0800

    added get users function

--- 100 Days of Code - The Complete Python Pro Bootcamp/day-40/data_manager.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/day-40/data_manager.py
@@ -6,10 +6,9 @@ USERNAME_SHEETY = os.environ.get("ENV_SHEETY_USERNAME")
 BEARER_TOKEN = os.environ.get("ENV_SHEETY_BEARER_TOKEN")
 ENDPOINT = "https://api.sheety.co"
 PROJECT_NAME = "krlovesFlightdeals"
-SHEET_NAME = "prices"
 # ----------------------------------------------------------#
 
-url_flight_deals = f"{ENDPOINT}/{USERNAME_SHEETY}/{PROJECT_NAME}/{SHEET_NAME}"
+url_flight_deals = f"{ENDPOINT}/{USERNAME_SHEETY}/{PROJECT_NAME}/"
 
 headers = {
     "Authorization": f"Bearer {BEARER_TOKEN}"
@@ -19,15 +18,24 @@ headers = {
 class DataManager:
     # This class is responsible for talking to the Google Sheet.
     def __init__(self):
-        self.endpoint = url_flight_deals
+        self.endpoint_prices = url_flight_deals+'prices'
+        self.endpoint_users = url_flight_deals + 'users'
 
     def get_data(self):
-        request = requests.get(url=self.endpoint,
+        request = requests.get(url=self.endpoint_prices,
                                headers=headers)
         request.raise_for_status()
         response = request.json()
 
-        return response
+        return response['prices']
+
+    def get_users(self):
+        request = requests.get(url=self.endpoint_users,
+                               headers=headers)
+        request.raise_for_status()
+        response = request.json()
+
+        return response['users']
 
     def update_row_iata(self, index, iata):
         body = {
@@ -35,9 +43,8 @@ class DataManager:
                 "iataCode": iata
             }
         }
-        request = requests.put(url=f"{self.endpoint}/{str(index)}",
+        request = requests.put(url=f"{self.endpoint_prices}/{str(index)}",
                                json=body,
                                headers=headers)
         response = request.json()
         return response
-
