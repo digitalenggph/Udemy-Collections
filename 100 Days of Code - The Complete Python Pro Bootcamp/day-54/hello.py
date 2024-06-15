3d96d6d35066626b6a60e16bbaedaf767b3d3b2d
digitalenggph <karen.lovedorial@gmail.com>
2024-06-15 22:33:56 +0800

    finished day-54

--- 100 Days of Code - The Complete Python Pro Bootcamp/day-54/hello.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/day-54/hello.py
@@ -1,6 +1,6 @@
 """`
 DAY-54 NOTES
-
+    LESSON 1:
     * pwd   - print working directory
     * ls    - list all files and folders in current directory
     * cd    - change directory
@@ -11,11 +11,14 @@ DAY-54 NOTES
                 -rf     - flag (dash) rf
                         - recursively deletes everything
 
-
+    LESSON 2:
+    __name___
 `"""
 
-
 # save this as hello.py
+
+print(__name__)
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -26,11 +29,10 @@ def hello():
     return "Hello, World!"
 
 
-hello()
+@app.route("/bye")
+def say_bye():
+    return "Bye"
 
 
-
-
-
-
-
+if __name__ == "__main__":
+    app.run()
