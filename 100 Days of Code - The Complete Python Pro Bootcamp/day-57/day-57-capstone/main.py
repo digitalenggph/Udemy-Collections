2177cb4d1c194b79da68dea2323aafdb0b29308d
digitalenggph <karen.lovedorial@gmail.com>
2024-06-21 02:14:53 +0800

    working solution before peeking at instructor's code :'>

--- 100 Days of Code - The Complete Python Pro Bootcamp/day-57/day-57-capstone/main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/day-57/day-57-capstone/main.py
@@ -1,11 +1,25 @@
 from flask import Flask, render_template
-
+from post import Post
 
 app = Flask(__name__)
 
-@app.route('/')
+blog_post = Post()
+posts = blog_post.get_blog()
+
+
+@app.route('/blog')
 def home():
-    return render_template("index.html")
+    return render_template("index.html",
+                           posts=posts)
+
+
+@app.route('/post/<blog_id>')
+def get_blog(blog_id):
+    print(blog_id)
+    return render_template('post.html',
+                           posts=posts,
+                           blog_id=blog_id)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- 100 Days of Code - The Complete Python Pro Bootcamp/day-57/day-57-capstone/post.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/day-57/day-57-capstone/post.py
@@ -1,2 +1,14 @@
+import requests
+
+
 class Post:
-    pass
+    def __init__(self):
+        self.url = 'https://api.npoint.io/f14658c6e58c834c01e0'
+
+    def get_blog(self):
+        blog_data = requests.get(url=self.url)
+        blog_data.raise_for_status()
+        posts = blog_data.json()
+        return posts
+
+
