a10d783705867565965137836a55655f03aac225
digitalenggph <karen.lovedorial@gmail.com>
2024-06-24 22:59:54 +0800

    finished step 5

--- 100 Days of Code - The Complete Python Pro Bootcamp/day-59/day-59-capstone-upgraded-blog/main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/day-59/day-59-capstone-upgraded-blog/main.py
@@ -8,5 +8,16 @@ def home():
     return render_template('index.html')
 
 
+@app.route('/<nav_page>')
+def get_nav(nav_page):
+    if nav_page.lower() == 'about':
+        template = 'about.html'
+    elif nav_page.lower() == 'contact':
+        template = 'contact.html'
+    else:
+        template = 'index.html'
+    return render_template(template, nav_page=nav_page)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
