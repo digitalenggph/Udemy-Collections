2091b62ef2c0bff8e5ddf4f5090c13efe8856399
digitalenggph <101250873+digitalenggph@users.noreply.github.com>
2025-03-30 21:25:16 +0800

    learn basics of PHP

---
+++ Modern PHP - The Complete Guide - from Beginner to Advanced/01-first-steps.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php echo 'Hello from PHP!' ?>
+    <h1>Hi, My name is Karen!</h1>
+    <h1>A dice roll: <?php echo rand(1,6); ?></h1>
+</body>
+</html>
