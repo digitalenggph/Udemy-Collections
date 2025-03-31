f31352f7dff1e6fb2ae12e9cd8df19eb392479df
digitalenggph <101250873+digitalenggph@users.noreply.github.com>
2025-03-31 14:15:55 +0800

    learn about errors

---
+++ Modern PHP - The Complete Guide - from Beginner to Advanced/09-errors/errors.php
@@ -0,0 +1,13 @@
+<?php
+
+echo '....';
+
+$pageTitle = 'A restaurant website';
+echo $pageTitle
+
+// Parse error > Fatal error > Warning
+// Parse -> will not run at all
+// Fatal -> will run but will stop at point of error
+// Warning -> will still continue but will return error message
+
+?>
