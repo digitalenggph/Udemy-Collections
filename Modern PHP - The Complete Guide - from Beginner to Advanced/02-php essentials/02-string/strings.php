db8f3e390690662ee3096c83bdc11e11a9c0b4a6
digitalenggph <101250873+digitalenggph@users.noreply.github.com>
2025-04-08 13:38:00 +0800

    copy php training files to github

---
+++ Modern PHP - The Complete Guide - from Beginner to Advanced/02-php essentials/02-string/strings.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="./simple.css" />
+    <title>Document</title>
+</head>
+<body>
+    <pre><?php
+
+
+$greeting = "I'm a string";
+echo "{$greeting}_____!!!";
+echo '<br />';
+
+$name = 'Karen';
+$subject = 'PHP';
+
+$text =  "I'm {$name} and \n I'm learning \t {$subject}.";
+echo $text;
+
+    ?></pre>
+</body>
+</html>
---
+++ Modern PHP - The Complete Guide - from Beginner to Advanced/02-php essentials/20-restaurant/inc/header.inc.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/simple.css" />
+    <link rel="stylesheet" href="./styles/custom.css" />
+    <title>Culinary Cove &bull; <?php echo $pageTitle; ?></title>
+</head>
+<body>
+  <?php 
+    if (empty($headerImg)) {
+      $headerImg = "images/pexels-julia-volk-5273044.jpg" ;
+    }
+  ?>
+
+  <header class="header-with-background" style="background-image: url('<?php echo $headerImg; ?>'); ">
+    <h1>Culinary Cove</h1>
+    <p>Your sanctuary for exceptional flavors</p>
+    <nav>
+        <a class="<?php if (!empty($pageKey) && $pageKey === "mission") echo 'active'; ?>" href="our-mission.php">Our mission</a>
+        <a class="<?php if (!empty($pageKey) && $pageKey === "ingredients") echo 'active'; ?>" href="ingredients.php">Ingredients</a>
+        <a class="<?php if (!empty($pageKey) && $pageKey === "menu") echo 'active'; ?>" href="menu.php">Menu</a>
+      
+    </nav>
+  </header>
+
+  <main>
