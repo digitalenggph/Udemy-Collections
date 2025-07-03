615ebdc16fb4309ffb76fb5f53d807389508b6bc
krloves <karen.lovedorial@gmail.com>
2025-07-03 20:49:45 +0800

    test __repr__

--- 100 Days of Code - The Complete Python Pro Bootcamp/day-63/day-63-database/main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/day-63/day-63-database/main.py
@@ -32,14 +32,21 @@ class Book(db.Model):
     author = Column(String(250), nullable=False)
     rating = Column(Float, nullable=False)
 
+    def __repr__(self):
+        return f"{{'title': '{self.title}', 'author': '{self.author}', 'rating': {self.rating}}}"
+
 # configure the SQLite database, relative to the app instance folder
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///new-books-collection.db"
 # initialize the app with the extension
 db.init_app(app)
 
 
+# with app.app_context():
+#     db.create_all()
+#     new_book = Book(title="Hunger Games", author="Suzanne Collins", rating=7.3)
+#     db.session.add(new_book)
+#     db.session.commit()
+#
 with app.app_context():
-    db.create_all()
-    new_book = Book(title="Harry Potter", author="J. K. Rowling", rating=9.3)
-    db.session.add(new_book)
-    db.session.commit()
+    result = db.session.execute(db.select(Book)).scalars().all()
+    print(len(result))
