494669cbceaa1fe415d09efd78057cbc67aeaac1
krloves <karen.lovedorial@gmail.com>
2025-07-04 16:15:08 +0800

    update home and edit route

--- 100 Days of Code - The Complete Python Pro Bootcamp/day-64/day-64-files-top-movies/main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/day-64/day-64-files-top-movies/main.py
@@ -1,8 +1,9 @@
+import sqlalchemy
 from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import Integer, String, Float
+from sqlalchemy import Integer, String, Float, result_tuple
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
@@ -27,14 +28,76 @@ Bootstrap5(app)
 
 # CREATE DB
 
+class Base(DeclarativeBase):
+    pass
+
+db = SQLAlchemy(model_class=Base)
+
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///top-10-movies.db"
+# initialize the app with the extension
+db.init_app(app)
 
 # CREATE TABLE
+
+class Movie(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    year: Mapped[int] = mapped_column(nullable=False)
+    description: Mapped[str] = mapped_column(nullable=False)
+    rating: Mapped[float] = mapped_column(nullable=False)
+    ranking:Mapped[int] = mapped_column(nullable=False)
+    review: Mapped[str] = mapped_column(nullable=False)
+    img_url: Mapped[str] = mapped_column(nullable=False)
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    result = db.session.execute(
+                db.select(Movie).order_by(sqlalchemy.desc(Movie.ranking))
+                ).scalars().all()
+    return render_template("index.html", all_movies=result)
 
+
+@app.route("/edit/<movie_id>", methods=["GET", "POST"])
+def edit(movie_id):
+    return render_template("edit.html")
+
+# -------------------- ADD FIRST NEW MOVIE -------------------- #
+
+# with app.app_context():
+# new_movie = Movie(
+#     title="Phone Booth",
+#     year=2002,
+#     description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
+#     rating=7.3,
+#     ranking=10,
+#     review="My favourite character was the caller.",
+#     img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
+# )
+# db.session.add(new_movie)
+# db.session.commit()
+
+# -------------------- ADD SECOND NEW MOVIE -------------------- #
+
+# with app.app_context():
+#     second_movie = Movie(
+#         title="Avatar The Way of Water",
+#         year=2022,
+#         description="Set more than a decade after the events of the first film, learn the story of the Sully family (Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure.",
+#         rating=7.3,
+#         ranking=9,
+#         review="I liked the water.",
+#         img_url="https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg"
+#     )
+#
+#     db.session.add(second_movie)
+#     db.session.commit()
+
+# -------------------------------------------------------------- #
+
+# with app.app_context():
+#     db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=True)
