af31689931295c97e96d0a8a9c6ebc44f43c8efa
krloves <karen.lovedorial@gmail.com>
2025-07-29 20:13:27 +0800

    update files

---
+++ 100 Days of Code - The Complete Python Pro Bootcamp/day-83/main.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+from excel_to_html import dict_credentials, dict_project, dict_experience, dict_certifications
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+
+    return render_template('index.html',
+                           credentials=dict_credentials,
+                           project=dict_project,
+                           experience=dict_experience,
+                           certifications=dict_certifications)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=7777)
+
+
+
