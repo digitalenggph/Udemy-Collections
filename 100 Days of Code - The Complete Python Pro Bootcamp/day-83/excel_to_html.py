0114f56484d7094a9f8a7e62f559e03715abc55b
krloves <karen.lovedorial@gmail.com>
2025-08-04 23:53:34 +0800

    add socials sheet in excel_to_html.py

--- 100 Days of Code - The Complete Python Pro Bootcamp/day-83/excel_to_html.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/day-83/excel_to_html.py
@@ -6,11 +6,12 @@ print(sheets)
 
 dict_df = {sheet:pd.read_excel(io="Credentials.xlsx", sheet_name=sheet) for sheet in sheets}
 
-dict_credentials, dict_project, dict_experience, dict_certifications = \
+dict_credentials, dict_project, dict_experience, dict_certifications, dict_socials = \
     dict_df['Credentials'].to_dict(orient='records'),\
     dict_df['Project'].to_dict(orient='records'),\
     dict_df['Experience'].to_dict(orient='records'),\
-    dict_df['Certifications'].to_dict(orient='records')
+    dict_df['Certifications'].to_dict(orient='records'),\
+    dict_df['Socials'].to_dict(orient='records')
 
 for project in dict_project:
     project['Tools'] = [tool.strip() for tool in str(project['Tools']).split(',')]
