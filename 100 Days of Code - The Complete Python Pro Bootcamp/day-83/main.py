56cb685fddadd2dec42f92caa8f20e4b2f727f19
krloves <karen.lovedorial@gmail.com>
2025-10-13 22:36:22 +0800

    update credentials portion and add link for resume and full project archive

--- 100 Days of Code - The Complete Python Pro Bootcamp/day-83/main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/day-83/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
 from excel_to_html import excel_to_html
 
 dict_credentials, dict_project, dict_experience, dict_certifications, dict_socials = excel_to_html("Credentials.xlsx")
@@ -14,6 +14,15 @@ def home():
                            certifications=dict_certifications,
                            socials=dict_socials)
 
+@app.route('/resume')
+def experience():
+    file_path = "./static/assets/resume/trial.txt"
+    return send_file(file_path, as_attachment=True)
+
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html',)
 
 if __name__ == '__main__':
     app.run(debug=True, port=7777)
